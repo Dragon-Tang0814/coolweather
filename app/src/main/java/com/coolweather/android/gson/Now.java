afe15bf27afa634b7b42d31c3add95bfd8989664
Dragon <1740113847@qq.com>
2020-08-19 18:51:06 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -0,0 +1,14 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Now {
+    @SerializedName("tmp")
+    public String temperature;//温度
+    @SerializedName("cond")
+    public More more;
+    public class More {
+        @SerializedName("txt")
+        public String info;//晴,多云等...
+    }
+}
--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -4,6 +4,9 @@ import android.text.TextUtils;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
+import com.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -75,4 +78,15 @@ public class Utility {
         return false;
     }
 
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);//将JSON数据装换为Weather对象
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
